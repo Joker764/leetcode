53b33ae8cc806905e7734a5af3e30ed7da2ea948
hachi <764203174@qq.com>
2019-07-05 13:32:38 +0800

    add

---
+++ code/3.无重复字符的最长子串.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=3 lang=java
+ *
+ * [3] 无重复字符的最长子串
+ */
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        int ans = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < len; end++) {
+            char alpha = s.charAt(end);
+
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+
+        return ans;
+    }
+}
